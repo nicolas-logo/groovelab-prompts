38cfe3714089638907cffa8d7ae7841b90c70f95
Nicolás Logosettis <logo.ncls@gmail.com>
2024-12-25 16:12:01 -0300

    logo update

--- app/page.jsx
+++ app/page.jsx
@@ -5,11 +5,10 @@ const Home = () => (
     <h1 className='head_text text-center'>
       Discover & Share
       <br className='max-md:hidden' />
-      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+      <span className='orange_gradient text-center'> AI-Generated Music Samples</span>
     </h1>
     <p className='desc text-center'>
-      GrooveLab is an open-source AI prompting tool for modern world to
-      discover, create and share creative prompts
+    GrooveLab is an innovative platform where you can discover, create, and share AI-powered prompts to generate unique music samples. Explore the power of creativity with cutting-edge technology!
     </p>
 
     <Feed />
